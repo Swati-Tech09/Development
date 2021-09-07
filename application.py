6e2eccc0ae34eda4d5322bc08a4278b53491ba7b
Swati-Tech09 <80823399+Swati-Tech09@users.noreply.github.com>
2021-09-07 23:38:16 +0530

    Deploy ML

---
+++ application.py
@@ -0,0 +1,22 @@
+from flask import Flask,request
+import joblib
+application=Flask(__name__)
+
+@application.route('/')
+def hello_world():
+    return "Hello World"
+
+vectorizer=joblib.load("vectorizer.pkl")
+spamorham_model=joblib.load("spam_ham_model.pkl")
+
+
+@application.route('/spamorham',methods=['GET','POST'])
+def spamorham():
+    message=request.args.get("message")
+    vect_message=vectorizer.transform([message])
+    result=spamorham_model.predict(vect_message)[0]
+    return result
+
+
+if __name__=='__main__':
+    application.run()
